1f2220c3514e048a5b2c34bb33eb5b3c159aa0a1
Rakshit Agrawal <rakmak3456@gmail.com>
2025-03-27 14:14:40 -0400

    Base layout design

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { Container, Grid2 } from "@mui/material";
+import { Header } from "./components";
+
 function App() {
   return (
-    <h1>Dashboard page</h1>
+    <>
+      <Header />
+      <Container maxWidth="none" sx={{ flex: 1, overflow: "auto" }}>
+        <Grid2 container spacing={2}>
+          <Grid2 size={4}>Options will come here</Grid2>
+          <Grid2 size={8}>Graphs will come here</Grid2>
+        </Grid2>
+      </Container>
+    </>
   );
 }
 
